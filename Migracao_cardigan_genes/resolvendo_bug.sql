cb988f9d2ff8382488fd27b70670386a615dd5ce
Ester <esterrmb@gmail.com>
2018-07-20 10:30:08 -0300

    arrumando inputs para bugbox

--- Migracao_cardigan_genes/resolvendo_bug.sql
+++ Migracao_cardigan_genes/resolvendo_bug.sql
@@ -1,5 +1,6 @@
-select * from gene g, coordenada_gene c, gene_nomeia_coordenada n 
-    where nome_no_banco = 'HNRNPCP5' 
+select g.*, c. from gene g, coordenada_gene c, gene_nomeia_coordenada n 
+    --where nome_no_banco = 'HNRNPCP5' 
+    where c.id_coordenada in (587, 64262)
     and g.id_gene = n.id_gene 
     and c.id_coordenada = n.id_coordenada
 ;
@@ -9,3 +10,8 @@ id_gene
 
 id_coordenada
 587
+
+
+\copy (select distinct(n.*) from gene g, coordenada_gene c, gene_nomeia_coordenada n  where c.id_coordenada in (587, 64262)     and g.id_gene = n.id_gene      and c.id_coordenada = n.id_coordenada ) to /home/ester/bugbox_nomeia_coordenadas.tsv csv delimiter e'\t'
+\copy (select distinct(c.*) from gene g, coordenada_gene c, gene_nomeia_coordenada n  where c.id_coordenada in (587, 64262)     and g.id_gene = n.id_gene      and c.id_coordenada = n.id_coordenada ) to /home/ester/bugbox_nomeia_coordenadas.tsv csv delimiter e'\t'
+\copy (select distinct(g.*) from gene g, coordenada_gene c, gene_nomeia_coordenada n  where c.id_coordenada in (587, 64262)     and g.id_gene = n.id_gene      and c.id_coordenada = n.id_coordenada ) to /home/ester/bugbox_nomeia_coordenadas.tsv csv delimiter e'\t'
